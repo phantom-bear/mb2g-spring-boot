564e6b19e0505ed9541049e09e1a646b41d9fe24
phantom-bear <phantombear6650@gamil.com>
2024-07-17 23:36:55 +0800

    mm spring boot

---
+++ web-model/src/main/java/guru/springframework/springboot/model/User.java
@@ -0,0 +1,33 @@
+package guru.springframework.springboot.model;
+
+public class User {
+
+    private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    private String lastName;
+    private String email;
+}
